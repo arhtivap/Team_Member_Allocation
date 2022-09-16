33aecc133c4013dbc4b57d8c59d0c056e8983dca
Pavithra Sunkesulu <pavi.kec@gmail.com>
2022-09-16 14:22:59 -0300

    Added Employees.jsx file.

---
+++ src/Employees.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const Employees = () => {
+
+    const [employees, setEmployees] = useState();
+    return (
+        <main>
+
+        </main>
+    ) 
+}
+
+export default Employees
